8a9305e337d7678b87e821bc240bc40499c76262
Zach Ioannou <zioannou@andronisexclusive.com>
2018-11-08 18:22:07 +0200

    replaced products list with service constructor initialization

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product-service';
 
 @Component({
     selector: 'pm-products',
@@ -21,47 +22,12 @@ export class ProductListComponent implements OnInit {
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            'productId': 1,
-            'productName': 'Leaf Rake',
-            'productCode': 'GDN-0011',
-            'releaseDate': 'March 19, 2016',
-            'description': 'Leaf rake with 48-inch wooden handle.',
-            'price': 19.95,
-            'starRating': 3.2,
-            'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-          },
-          {
-            'productId': 2,
-            'productName': 'Garden Cart',
-            'productCode': 'GDN-0023',
-            'releaseDate': 'March 18, 2016',
-            'description': '15 gallon capacity rolling garden cart',
-            'price': 32.99,
-            'starRating': 4.2,
-            'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-          },
-          {
-            'productId': 5,
-            'productName': 'Hammer',
-            'productCode': 'TBX-0048',
-            'releaseDate': 'May 21, 2016',
-            'description': 'Curved claw steel hammer',
-            'price': 8.9,
-            'starRating': 4.8,
-            'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
-          }
-    ];
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    products: IProduct[];
+    constructor(private productService: ProductService) {
+        this.listFilter = '';
     }
     toggleImage(): void {
         this.showImage = !this.showImage;
-    }
-    ngOnInit(): void {
-        console.log('In OnInit');
     }
     onRatingClicked(message: string): void {
         this.pageTitle = 'Product List: ' + message;
@@ -70,6 +36,10 @@ export class ProductListComponent implements OnInit {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
             product.productName.toLowerCase().indexOf(filterBy) !== -1);
+    }
+    ngOnInit(): void {
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 }
 
