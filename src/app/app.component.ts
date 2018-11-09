9c9c8b6af49df2fbafe3d032a5509224273a1f17
Zach Ioannou <zioannou@andronisexclusive.com>
2018-11-09 16:58:43 +0200

    added basic routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,24 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `<div>
-    <h1>{{pageTitle}}</h1><br>
-    <pm-products></pm-products>
-  </div>`
+  template: `
+  <nav class='navbar navbar-expand navbar-light bg-light'>
+    <a class='navbar-brand'>{{pageTitle}}</a>
+    <ul class='nav nav-pills'>
+      <li><a class='nav-link' (click)='ngMenuClick($event)' [routerLink]="['/welcome']">Home</a></li>
+      <li><a class='nav-link' (click)='ngMenuClick($event)' [routerLink]="[ '/products']">Product List</a></li>
+    </ul>
+  </nav>
+  <div class='container'>
+    <router-outlet></router-outlet>
+  </div>
+  `
 })
 
 export class AppComponent {
   pageTitle = 'Angular Startup Project';
   bodyTitle = 'this is the body of our components';
+  ngMenuClick(event): void {
+    console.log(`clicked ${event.currentTarget.text}`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -9,6 +10,7 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +24,14 @@ import { WelcomeComponent } from './home/welcome.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent},
+      { path: 'product/:id', component: ProductDetailComponent},
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'},
+    ])
   ], // angular itself or 3rd parties
   bootstrap: [AppComponent]
 })
