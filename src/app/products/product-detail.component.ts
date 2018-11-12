0622042e915abe9239ed3ca9bea26bb8b51cfeeb
Zach Ioannou <zioannou@andronisexclusive.com>
2018-11-12 12:54:03 +0200

    added product detail navigation routing

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pateTitle = 'Product Detail';
+  pageTitle = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor (private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2016',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    };
   }
-
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
